214d83170a6ee479e9e4aa22901e9602b5fd5099
Clockbomb <mrgo2000@gmail.com>
2021-11-24 19:01:37 +0100

    exercise2.6

--- DM_topic/DM_assignment.py
+++ DM_topic/DM_assignment.py
@@ -3,18 +3,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns; sns.set_theme(color_codes=True)
 import statsmodels.api as sm
+import os
+
+# Exercise 2.6
+
+path = os.path.dirname(os.path.realpath(__file__)) + '/voorbeeld7_1.sav'
+chol1 =  pd.read_spss(path)
 
 
-#path = __file__ + '/voorbeeld7_1.sav'
+# a)
+graph = sns.lmplot(x ='leeftijd', y ='chol', data = chol1, fit_reg=True)
 
-data_frame =  pd.read_spss('D:\\DataScience_Project\\DM_topic\\voorbeeld7_1.sav')
-
-#print(data['chol'])
-
-graph = sns.lmplot(x ='leeftijd', y ='chol', data = data_frame, fit_reg=True)
-plt.show()
-
-
-model = sm.formula.api.ols(formula = "chol ~ leeftijd",data = data_frame)¶
+# b)
+model = sm.formula.ols(formula = "chol~leeftijd", data = chol1)
 fit1 = model.fit()
-fit1.summary()
+print(fit1.summary())
+
+# c)
+fit2 = sm.formula.ols(formula = "chol ~ leeftijd + bmi + sekse + alcohol", data = chol1).fit()
+#print(fit2.summary())
+# Which factors are statiscally significant?
+
+# d)
+chol1['Residuals'] = fit2.resid
+
+plt.figure()
+sns.histplot(chol1['Residuals'])
+
+plt.show()
